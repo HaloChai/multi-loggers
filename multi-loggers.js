86ba5584ed4c9fab53925ca60d29371068bda418
jeff <jeff@feltmeng.com>
2013-12-13 11:25:24 +0800

    Create new function: exception catch.

--- multi-loggers.js
+++ multi-loggers.js
@@ -2,12 +2,20 @@
 var ml = {};
 
 ml.logger = require('./lib/logger.js');
+ml.exception = require('./lib/exception.js');
 
 ml.init = function( options ) {
     var body = {};
     options.transports.forEach(function( log ){
         body[log.method] = log[log.method];
     });
+    
+    if( options.exceptionCatch ) {
+        new ml.exception.listener({ 
+            show : ((options.exceptionCatch.show) === 'undefined') ? false : options.exceptionCatch.show, 
+            file : options.exceptionCatch.file 
+        });
+    }
     
     return body;
 };
--- test/test.js
+++ test/test.js
@@ -1,12 +1,19 @@
 
-var ml = require('multi-loggers');
+var ml = require('../multi-loggers');
 
 var logger = new ml.init({
     transports : [
         new ml.logger.setting({ method : 'info', file : 'info.log' }),
         new ml.logger.setting({ method : 'err', file : 'err.log' }),
-    ]
+    ],
+    exceptionCatch : { show : false, file : 'exception.log' }
 });
 
+// Coustom method
 logger.info('test info.');
 logger.err('test err.');
+
+// Catch exception
+setTimeout(function() {
+    console.log(test.name);
+}, 2000);
